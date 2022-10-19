3c7b15b0c769c3077619eb7581dfd3288604c7c1
Suman V <71666516+sumvel@users.noreply.github.com>
2022-10-19 18:20:21 +0530

    m8 results

--- src/js/data.js
+++ src/js/data.js
@@ -5,7 +5,7 @@ var r4_end = 42;
 var r3_exists = true;
 var r4_exists = false;
 
-var match_result = new Array("NAM", "NED", "SCOT", "ZIM", "NED", "SL", "IRE"); //"SCOT", "IRE"
+var match_result = new Array("NAM", "NED", "SCOT", "ZIM", "NED", "SL", "IRE", "WI"); //"SCOT", "IRE"
 //match_result[0] = "NAM";
 //match_result[1] = "NED";
 var mat_per_week = new Array(0, 14, 28, 41); // included
